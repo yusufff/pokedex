aebaeb0bf854c8c6d4ba008e38165660dfa90eee
ysfu <a.yusuf.yildiz@hotmail.com>
2019-08-31 19:16:20 +0300

    Updates

---
+++ precache-manifest.dd39d05ef24ebdf42c9897764dd6ec9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fc06b479f7e016b8152d871efff928",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "fda69586da6a93f64939",
+    "url": "/pokedex/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8b190584cbcd448d6349",
+    "url": "/pokedex/static/css/main.9ff970aa.chunk.css"
+  },
+  {
+    "revision": "fda69586da6a93f64939",
+    "url": "/pokedex/static/js/2.82d55432.chunk.js"
+  },
+  {
+    "revision": "8b190584cbcd448d6349",
+    "url": "/pokedex/static/js/main.a3c0c861.chunk.js"
+  },
+  {
+    "revision": "e2590d326f0902d7440b",
+    "url": "/pokedex/static/js/runtime~main.9b58ce06.js"
+  }
+]);
